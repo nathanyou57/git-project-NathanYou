4a5dbba33cd75db040ac4d0dbdd8078301ca6e77
Samuel Benardo <sbenardo1@hwemail.com>
2025-10-12 23:51:16 -0700

    (GP-4.3) Wrote README.md
    I believe the tester does a "full cycle of git" but if not the instructions are unclear/vague.

--- GitWrapperTester.java
+++ GitWrapperTester.java
@@ -5,12 +5,12 @@ import java.security.NoSuchAlgorithmException;
 public class GitWrapperTester {
     public static void main(String args[]) throws IOException, NoSuchAlgorithmException {
         Git.initializeRepo();
-        /* Your tester code goes here */
+
         GitWrapper gw = new GitWrapper();
         gw.add("myProgram/inner/world.txt");
         gw.add("myProgram/hello.txt");
         gw.commit("John Doe", "Initial commit");
-        gw.checkout("1234567890");
+        //gw.checkout("1234567890");
 
     }
 
